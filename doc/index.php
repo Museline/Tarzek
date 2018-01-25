e14ddb45dfaf7ab5274a41aaad70508a2d2513c6
Khael77 <khael77@gmail.com>
2018-01-25 11:25:09 +0100

    Html de test (doc/index.php) + librairies JS (jquery + plugin) + code pour les fonction du jeu comme sur la page 244 du tuto

--- doc/index.php
+++ doc/index.php
@@ -7,10 +7,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<title>Test Jeu</title>
+        <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+        <link rel="stylesheet" type="text/css" href="../assets/css/jeu.css">
+        <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="../assets/js/jquery.backgroundpos.min.js"></script>
+        <script type="text/javascript" src="../assets/js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="../assets/js/jquery.path.js"></script>
+        <script type="text/javascript" src="../assets/js/jeux.js"></script>
 </head>
 <body>
-
+    <div id="container">
+        <div id="legend"></div>
+        <div id="perso"></div>
+        <div class="case c1"></div>
+        <div class="case c2"></div>
+        <div class="case c3"></div>
+        <div class="smile s1"></div>
+        <div class="smile s2"></div>
+        <div class="smile s3"></div>
+        <div class="ground"></div>
+    </div>
 </body>
 </html>
