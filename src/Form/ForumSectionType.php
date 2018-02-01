e2bda0ca9411c03ea50d0160d9eec6244a265cdb
Museline <info.museline@gmail.com>
2018-02-01 12:08:11 +0100

    Début de la création des sections par l'admin et déplacement des vues liés au forum dans un dossier forum

---
+++ src/Form/ForumSectionType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ForumSectionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('section_name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('access', IntegerType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // uncomment if you want to bind to a class
+            //'data_class' => ForumSection::class,
+        ]);
+    }
+}
