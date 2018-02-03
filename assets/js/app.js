9125cca55d708e273ff9aa536ed9a1fb0a611310
Museline <info.museline@gmail.com>
2018-02-03 14:59:05 +0100

    Base css et mise à jour js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
-// require css
-require('../css/jeu.css');
+// require css base
+
+// require css module
+require('../css/module/jeu.css');
 
 // charge jquery depuis les package le node_modules
 var $ = require('jquery');
@@ -10,6 +12,7 @@ var game = require('./jeux.js');
 var jquery_back = require('./jquery.backgroundpos');
 var jquery_easing = require('./jquery.easing.1.3');
 var jquery_path = require('./jquery.path');
+var multi_step_form = require('./multi_step_form');
 
 // une fois le document prêt, lance les scripts js
 $(document).ready(function() {
@@ -18,4 +21,5 @@ $(document).ready(function() {
     jquery_back();
     jquery_easing();
     jquery_path();
+    multi_step_form();
 });
